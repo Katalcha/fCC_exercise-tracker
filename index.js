5a1b14455657cf7b19b3ae0a03b95f087c471faa
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-12-04 18:31:16 +0100

    feat: require mongoose and bodyParser

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
 app.use(cors());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.get('/api/ready', (req, res) => {
+  try {
+    res.json({ status: 'ready' });
+  } catch (err) {
+    res.json({ status: 'not ready' });
+  }
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+  console.log(`Your app is listening on port ${listener.address().port}`);
 });
